d4d20109fe392ed1b92cd2ebf382eb05efddb2c3
Boshi Yuan <542629877@qq.com>
2025-01-15 22:25:08 +0800

    Add premulc

--- Compiler/comparison_mersenne.py
+++ Compiler/comparison_mersenne.py
@@ -13,16 +13,17 @@ def LTZ(a):
     from .instructions import bit
     from .comparison import BitLTL, BitLTC1
     from .comparison import const_rounds
+    from .library import print_ln, print_str
 
-    # print(const_rounds)
-    
+
     bit_length = program.Program.prog.options.mersenne
 
     r_bits = [sintbit() for _ in range(bit_length)]
     for i in range(bit_length):
         bit(r_bits[i])
     r = sint.bit_compose(r_bits)
-    y = (r + 2 * a).reveal(check=False)
+
+    y = (r + 2 * a).reveal(check=False) # y = 2a + r
     y_lsb = y & 1
     b = y_lsb + r_bits[0] - 2 * r_bits[0] * y_lsb # b = y_lsb ^ r[0]
     
@@ -30,7 +31,6 @@ def LTZ(a):
 
     if const_rounds:
         c = BitLTC(y, r_bits)
-        # BitLTC1(c, y, r_bits)
     else:
         BitLTL(c, y, r_bits)
 
@@ -45,7 +45,7 @@ def BitLTC(a, b):
     b: array of sintbit
     '''
     from .types import sintbit
-    from .comparison import PreMulC
+    from .library import print_ln, print_str
 
     k = len(b)
     a_bits = floatingpoint.bits(a, k)
@@ -54,14 +54,39 @@ def BitLTC(a, b):
     b_bits_not = [x.bit_not() for x in b]
 
     # c_i = a_not_i ^ b_not_i
-    c_bits = [x + y - 2 * x * y for x, y in zip(a_bits_not, b_bits_not)]
+    c_bits = [lhs + rhs - 2 * lhs * rhs for lhs, rhs in zip(a_bits_not, b_bits_not)]
     c_bits_not = [x.bit_not() for x in c_bits]
-    d_bits = PreMulC(c_bits_not)
+
+    d_bits = PreMulC(list(reversed(c_bits_not)))
+    d_bits.reverse()
     d_bits_not = [x.bit_not() for x in d_bits]
 
     e = [d_bits_not[i] - d_bits_not[i + 1] for i in range(k - 1)]
     e.append(d_bits_not[k - 1])
 
-    res = sum(lhs * rhs for lhs, rhs in zip(a_bits, e))
+    return sum(lhs * rhs for lhs, rhs in zip(a_bits_not, e))
 
+
+def PreMulC(a):
+    '''
+    a: array of sint
+    '''
+    from .types import sint
+    from .instructions import inverse
+
+    k = len(a)
+
+    r = [sint() for _ in range(k)]
+    s = [sint() for _ in range(k)]
+    for i in range(k):
+        inverse(r[i], s[i])
+    t = [s[0]] + [r[i - 1] * s[i] for i in range(1, k)]
+    c = [a[i] * t[i] for i in range(k)]
+    for c_i in c:
+        c_i = c_i.reveal(check=False)
+    c_mult = 1
+    res = []
+    for i in range(k):
+        c_mult *= c[i]
+        res.append(c_mult * r[i])
     return res
