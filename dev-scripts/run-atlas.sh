06ae816d071c878071583abba3b67868fe1a9ae9
Boshi Yuan <542629877@qq.com>
2025-01-18 16:57:13 +0800

    add some scripts for benchmarking

---
+++ dev-scripts/run-atlas.sh
@@ -0,0 +1,34 @@
+HERE=$(cd `dirname $0`; pwd)
+SPDZROOT=$HERE/..
+
+
+# Check if correct number of arguments provided
+if [ "$#" -ne 4 ]; then
+    echo "Usage: $0 <progname> <number_of_parties> <start_party> <end_party>"
+    exit 1
+fi
+
+progname=$1
+nparties=$2
+start=$3
+end=$4
+port=12345
+
+server1=192.168.0.25
+server2=192.168.0.169
+
+$SPDZROOT/compile.py $progname
+
+# if [ $start -eq 0 ]; then
+#     scp -r $SPDZROOT/Programs $server1:$SPDZROOT
+# fi
+
+for i in $(seq $start $end); do
+    echo "Running $SPDZROOT/atlas-party.x -pn $port -N $nparties $i $progname"
+
+    if [ $i -eq $end ]; then
+        $SPDZROOT/atlas-party.x -pn $port -N $nparties -h $server1 $i $progname
+    else
+        $SPDZROOT/atlas-party.x -pn $port -N $nparties -h $server1 $i $progname >/dev/null 2>&1 &
+    fi
+done
