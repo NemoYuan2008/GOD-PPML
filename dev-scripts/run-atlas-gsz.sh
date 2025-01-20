c8b389b61a480fd6ed52530d269a916b3424d4b4
Boshi Yuan <542629877@qq.com>
2025-01-20 20:54:20 +0800

    update scripts

--- dev-scripts/run-atlas-gsz.sh
+++ dev-scripts/run-atlas-gsz.sh
@@ -1,8 +1,8 @@
+#!/usr/bin/env bash
+
 HERE=$(cd `dirname $0`; pwd)
 SPDZROOT=$HERE/..
 
-
-# Check if correct number of arguments provided
 if [ "$#" -ne 4 ]; then
     echo "Usage: $0 <progname> <number_of_parties> <start_party> <end_party>"
     exit 1
@@ -14,17 +14,13 @@ start=$3
 end=$4
 port=12345
 
-server1=192.168.0.25
+server1=192.168.0.32
 server2=192.168.0.169
 
-$SPDZROOT/compile.py $progname
-
-# if [ $start -eq 0 ]; then
-#     scp -r $SPDZROOT/Programs $server1:$SPDZROOT
-# fi
+$SPDZROOT/compile.py $progname --budget 1000000
 
 for i in $(seq $start $end); do
-    echo "Running $SPDZROOT/atlas-gsz-party.x -pn $port -N $nparties $i $progname"
+    echo "Running $SPDZROOT/atlas-gsz-party.x -pn $port -N $nparties -h $server1 $i $progname"
 
     if [ $i -eq $end ]; then
         $SPDZROOT/atlas-gsz-party.x -pn $port -N $nparties -h $server1 $i $progname
---
+++ dev-scripts/throttle-all.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+case "$1" in
+    "wan")
+        sudo tc qdisc del dev lo root >/dev/null 2>&1
+        sudo tc qdisc add dev lo root netem delay 40ms rate 100Mbps
+        sudo tc qdisc del dev eth0 root >/dev/null 2>&1
+        sudo tc qdisc add dev eth0 root netem delay 40ms rate 100Mbps
+        ;;
+    "lan")
+        sudo tc qdisc del dev lo root >/dev/null 2>&1
+        sudo tc qdisc add dev lo root netem delay 0.1ms rate 15Gbps
+        sudo tc qdisc del dev eth0 root >/dev/null 2>&1
+        sudo tc qdisc add dev eth0 root netem delay 0.1ms rate 15Gbps
+        ;;
+    "reset")
+        sudo tc qdisc del dev lo root
+        sudo tc qdisc del dev eth0 root
+        ;;    
+    "custom")
+        sudo tc qdisc add dev lo root netem delay ${2}ms rate ${3}Mbit
+        sudo tc qdisc add dev eth0 root netem delay ${2}ms rate ${3}Mbit
+        ;;
+    *)
+        echo "OPTIONS: use 'wan', 'lan', 'reset' or 'custom [delay] [rate]'."
+        exit 1
+        ;;
+esac
