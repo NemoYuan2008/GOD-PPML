0f311d2542c2e26a515d5c7c8dc8bee04506c5fa
Boshi Yuan <542629877@qq.com>
2025-05-26 14:34:31 +0800

    sleep in scripts

--- dev-scripts/run-all-2.sh
+++ dev-scripts/run-all-2.sh
@@ -9,9 +9,15 @@ protocol=$1
 progname=$2
 
 ./dev-scripts/run-$protocol.sh $progname 3 2 2
+sleep 3
 ./dev-scripts/run-$protocol.sh $progname 5 3 4
+sleep 3
 ./dev-scripts/run-$protocol.sh $progname 7 4 6
+sleep 3
 ./dev-scripts/run-$protocol.sh $progname 9 5 8
+sleep 3
 ./dev-scripts/run-$protocol.sh $progname 11 6 10
+sleep 3
 ./dev-scripts/run-$protocol.sh $progname 13 7 12
+sleep 3
 ./dev-scripts/run-$protocol.sh $progname 15 8 14
